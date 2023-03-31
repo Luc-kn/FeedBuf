e40b1e1df259a65c71718c29aab0e28501d978a9
Luc-kn <knoppen.luc@gmail.com>
2023-03-31 12:59:11 +0200

    design update en verbetering

---
+++ Feedbuff/Student_Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Feedbuff
+{
+    public partial class Student_Login : Form
+    {
+        public Student_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            StudentLoginRegister StudentRg = new StudentLoginRegister();
+            StudentRg.ShowDialog();
+            this.Close();
+        }
+            [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+            private static extern IntPtr CreateRoundRectRgn
+           (
+               int nLeftRect,     // x-coordinate of upper-left corner
+               int nTopRect,      // y-coordinate of upper-left corner
+               int nRightRect,    // x-coordinate of lower-right corner
+               int nBottomRect,   // y-coordinate of lower-right corner
+               int nWidthEllipse, // width of ellipse
+               int nHeightEllipse // height of ellipse
+          );
+        private void Student_Login_Load(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+    }
+}
--- Feedbuff/Welkom.cs
+++ Feedbuff/Welkom.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,17 +17,29 @@ namespace Feedbuff
         {
             InitializeComponent();
         }
+            [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+            private static extern IntPtr CreateRoundRectRgn
+         (
+             int nLeftRect,     // x-coordinate of upper-left corner
+             int nTopRect,      // y-coordinate of upper-left corner
+             int nRightRect,    // x-coordinate of lower-right corner
+             int nBottomRect,   // y-coordinate of lower-right corner
+             int nWidthEllipse, // width of ellipse
+             int nHeightEllipse // height of ellipse
+        );
 
         private void Welkom_Load(object sender, EventArgs e)
         {
-
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
 
         private void studentBtn_Click(object sender, EventArgs e)
         {
             this.Hide();
-            StudentLoginRegister Studentlr = new StudentLoginRegister();
-            Studentlr.ShowDialog();
+            Student_Login Studentlg= new Student_Login();
+            Studentlg.ShowDialog();
             this.Close();
         }
     }
